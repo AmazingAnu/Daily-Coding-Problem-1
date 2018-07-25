ff143ab30f49944f73507ce4ddb9cda190058ccc
Varun Upadhyay <varun.u28@gmail.com>
2018-07-24 20:41:03 -0700

    Added HasTargetSumPair.java

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Daily-Coding-Problem
-Solutions in Java for problems from https://www.dailycodingproblem.com
+## Solutions in Java for programming interview problems from [Daily Coding Problem](https://www.dailycodingproblem.com)
+
+S.no | Problem | Solution | 
+--- | --- | --- |
+1 | Find if there is a pair with target sum in an array | []() | 
---
+++ src/HasTargetSumPair.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Varun Upadhyay (https://github.com/varunu28)
+ *
+ */
+
+/*Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?*/
+
+public class HasTargetSumPair {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 3, 7};
+        int targetSum = 17;
+
+        System.out.println(checkForTargetSumUsingHashSet(arr, targetSum));
+    }
+
+    private static boolean checkForTargetSumUsingHashSet(int[] arr, int targetSum) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(targetSum-num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+        return false;
+    }
+}
