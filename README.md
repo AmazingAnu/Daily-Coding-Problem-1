7df99287ca465377a2f20772523bef7ff2c9c67d
Varun Upadhyay <varun.u28@gmail.com>
2018-08-05 07:59:32 -0700

    Added ClimbStaircase.java

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ S.no | Problem | Solution |
 1 | Find if there is a pair with target sum in an array | [HasTargetSumPair.java](https://github.com/varunu28/Daily-Coding-Problem/blob/master/src/HasTargetSumPair.java) | 
 2 | Return an array with each element as product of all elements except self | [ProductOfAllExceptSelf.java](https://github.com/varunu28/Daily-Coding-Problem/blob/master/src/ProductOfAllExceptSelf.java) | 
 3 | Get lowest positive integer not present in array | [LowestPositiveIntegerNotInArray.java](https://github.com/varunu28/Daily-Coding-Problem/blob/master/src/LowestPositiveIntegerNotInArray.java) | 
+4 | Find number of ways to climb a staircase | [ClimbStaircase.java](https://github.com/varunu28/Daily-Coding-Problem/blob/master/src/ClimbStaircase.java) | 
---
+++ src/ClimbStaircase.java
@@ -0,0 +1,66 @@
+/**
+ *
+ * @author Varun Upadhyay (https://github.com/varunu28)
+ *
+ */
+
+/*There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a
+function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+
+For example, if N is 4, then there are 5 unique ways:
+
+1, 1, 1, 1
+2, 1, 1
+1, 2, 1
+1, 1, 2
+2, 2
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive
+integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.*/
+
+public class ClimbStaircase {
+
+    public static void main(String[] args) {
+        System.out.println(findNumberOfWays(4));
+
+        int[] steps = {1,2};
+        System.out.println(findNumberOfWaysForGivenSteps(4, steps));
+    }
+
+    private static int findNumberOfWays(int n) {
+        int[] memo = new int[n+1];
+        return climbStairs(memo, 0, n);
+    }
+
+    private static int climbStairs(int[] memo,  int i, int n) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+
+        memo[i] = climbStairs(memo, i+1, n) + climbStairs(memo, i+2, n);
+
+        return memo[i];
+    }
+
+    private static int findNumberOfWaysForGivenSteps(int n, int[] ways) {
+        int[] memo = new int[n+1];
+        return climbStairsInGivenWays(memo, 0, n, ways);
+    }
+
+    private static int climbStairsInGivenWays(int[] memo, int i, int n, int[] ways) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+
+        for (int way : ways) {
+            memo[i] += climbStairsInGivenWays(memo, i+way, n, ways);
+        }
+
+        return memo[i];
+    }
+}
