e9d8c284ce7934cd871b65d2ff0fe6d15a1b1c66
varunu28 <varun.u28@gmail.com>
2019-04-19 07:51:24 -0700

    Added BinaryTreeBoustrophedonOrder.java

--- src/main/java/BinaryTreeBoustrophedonOrder.java
+++ src/main/java/BinaryTreeBoustrophedonOrder.java
@@ -20,11 +20,44 @@
 
 import utils.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTreeBoustrophedonOrder {
     public static List<Integer> getBoustrophedonOrder(TreeNode treeNode) {
-        return new ArrayList<>();
+        if (treeNode == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(treeNode);
+        int dir = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+
+            while (size-- > 0) {
+                TreeNode popped = queue.remove();
+                temp.add(popped.val);
+
+                if (popped.left != null) {
+                    queue.add(popped.left);
+                }
+
+                if (popped.right != null) {
+                    queue.add(popped.right);
+                }
+            }
+
+            if (dir % 2 != 0) {
+                Collections.reverse(temp);
+            }
+
+            list.addAll(temp);
+            dir++;
+        }
+
+        return list;
     }
 }
