b714a080880bde493c973e4a7eea4f641d4b5dfb
varunu28 <varun.u28@gmail.com>
2019-04-20 09:17:09 -0700

    Added GetAlternatingLinkedListOrder.java

--- src/main/java/GetAlternatingLinkedListOrder.java
+++ src/main/java/GetAlternatingLinkedListOrder.java
@@ -1,9 +1,11 @@
 /*
 * This problem was asked by Fitbit.
 *
-* Given a linked list, rearrange the node values such that they appear in alternating low -> high -> low -> high ... form.
+* Given a linked list, rearrange the node values such that they appear in alternating
+* low -> high -> low -> high ... form.
 *
-* For example, given 1 -> 2 -> 3 -> 4 -> 5, you should return 1 -> 3 -> 2 -> 5 -> 4.
+* For example, given 1 -> 2 -> 3 -> 4 -> 5, you should return 1 -> 5 -> 2 -> 4 -> 3.
+* given 1 -> 2 -> 3 -> 4 you should return 1 -> 4 -> 2 -> 3
 * */
 
 import utils.LinkedNode;
@@ -11,6 +13,61 @@ import utils.LinkedNode;
 public class GetAlternatingLinkedListOrder {
 
     public static LinkedNode getAlternatingOrder(LinkedNode head) {
-        return null;
+        if (head == null) {
+            return null;
+        }
+
+        LinkedNode slow = head;
+        LinkedNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        LinkedNode firstHalfStart = head;
+        LinkedNode secondHalfStart = reverseLinkedList(slow.next);
+        slow.next = null;
+
+        LinkedNode ans = new LinkedNode(-1);
+        LinkedNode nextNode = ans;
+        int count = 0;
+
+        while (firstHalfStart != null && secondHalfStart != null) {
+            if (count % 2 == 0) {
+                nextNode.next = new LinkedNode(firstHalfStart.val);
+                firstHalfStart = firstHalfStart.next;
+            }
+            else {
+                nextNode.next = new LinkedNode(secondHalfStart.val);
+                secondHalfStart = secondHalfStart.next;
+            }
+
+            nextNode = nextNode.next;
+            count++;
+        }
+
+        while (firstHalfStart != null) {
+            nextNode.next = new LinkedNode(firstHalfStart.val);
+            firstHalfStart = firstHalfStart.next;
+            nextNode = nextNode.next;
+        }
+
+        return ans.next;
+    }
+
+    private static LinkedNode reverseLinkedList(LinkedNode node) {
+        LinkedNode curr = node;
+        LinkedNode prev = null;
+        LinkedNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
 }
--- src/test/java/GetAlternatingLinkedListOrderTest.java
+++ src/test/java/GetAlternatingLinkedListOrderTest.java
@@ -1,19 +1,78 @@
 import org.junit.Test;
-import utils.LinkedListUtil;
 import utils.LinkedNode;
 
 import static junit.framework.TestCase.assertEquals;
+import static utils.LinkedListUtil.createLinkedList;
+import static utils.LinkedListUtil.printLinkedList;
 
 public class GetAlternatingLinkedListOrderTest {
 
-    LinkedNode head = LinkedListUtil.createLinkedList(new int[]{1, 2, 3, 4, 5});
-    LinkedNode ans = LinkedListUtil.createLinkedList(new int[]{1, 3, 2, 4, 5});
+    LinkedNode head;
+    LinkedNode ans;
 
     @Test
     public void test1() {
+        head = createLinkedList(new int[]{1, 2, 3, 4, 5});
+        ans = createLinkedList(new int[]{1, 5, 2, 4, 3});
+
         assertEquals(
-                LinkedListUtil.printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head)),
-                LinkedListUtil.printLinkedList(ans)
+                printLinkedList(ans),
+                printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head))
+        );
+    }
+
+    @Test
+    public void test2() {
+        head = createLinkedList(new int[]{1, 2, 3, 4});
+        ans = createLinkedList(new int[]{1, 4, 2, 3});
+
+        assertEquals(
+                printLinkedList(ans),
+                printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head))
+        );
+    }
+
+    @Test
+    public void test3() {
+        head = createLinkedList(new int[]{});
+        ans = createLinkedList(new int[]{});
+
+        assertEquals(
+                printLinkedList(ans),
+                printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head))
+        );
+    }
+
+    @Test
+    public void test4() {
+        head = createLinkedList(new int[]{1});
+        ans = createLinkedList(new int[]{1});
+
+        assertEquals(
+                printLinkedList(ans),
+                printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head))
+        );
+    }
+
+    @Test
+    public void test5() {
+        head = createLinkedList(new int[]{1, 2});
+        ans = createLinkedList(new int[]{1, 2});
+
+        assertEquals(
+                printLinkedList(ans),
+                printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head))
+        );
+    }
+
+    @Test
+    public void test6() {
+        head = createLinkedList(new int[]{1, 2, 3});
+        ans = createLinkedList(new int[]{1, 3, 2});
+
+        assertEquals(
+                printLinkedList(ans),
+                printLinkedList(GetAlternatingLinkedListOrder.getAlternatingOrder(head))
         );
     }
 }
